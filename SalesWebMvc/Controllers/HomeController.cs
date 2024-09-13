7555d4fb185f52549dfbe89316ce7ce8a394bcc1
Edmar Fonseca <edmarfonseca12@gmail.com>
2024-09-13 17:49:26 -0300

    SellersController

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -25,12 +25,7 @@ namespace SalesWebMvc.Controllers
 
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+                
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
